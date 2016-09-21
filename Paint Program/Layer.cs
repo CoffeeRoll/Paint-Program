6ee6d27ac72c05cc1411b53f55a356adae735b2d
JChouinard1198 <jchouinard@student.dwc.edu>
2016-09-21 17:28:18 -0400

    Add Layers Framework

---
+++ Paint Program/Layer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint_Program
+{
+    class Layer
+    {
+        private Bitmap LayerCanvas;
+        private bool isVisible;
+
+        public Layer(int w, int h, PixelFormat pf)
+        {
+            LayerCanvas = new Bitmap(w, h, pf);
+            isVisible = true;
+        }
+
+        public Bitmap getBitmap()
+        {
+            return LayerCanvas;
+        }
+
+        public void setBitmap(Bitmap b)
+        {
+            LayerCanvas = b;
+        }
+
+        public void setVisibility(bool b)
+        {
+            isVisible = b;
+        }
+
+        public bool isLayerVisible()
+        {
+            return isVisible;
+        }
+
+
+    }
+}
