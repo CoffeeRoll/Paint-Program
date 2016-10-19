ea70794b65ee483a0135b0f60f79dd2dfb86552d
JChouinard1198 <jchouinard@student.dwc.edu>
2016-10-19 17:27:39 -0400

    Start Layer Reordering

--- Paint Program/LayerView.Designer.cs
+++ Paint Program/LayerView.Designer.cs
@@ -31,11 +31,13 @@
             this.bAddLayer = new System.Windows.Forms.Button();
             this.bRemoveLayer = new System.Windows.Forms.Button();
             this.pLayerDisplay = new System.Windows.Forms.Panel();
+            this.bMoveDown = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bAddLayer
             // 
-            this.bAddLayer.Location = new System.Drawing.Point(112, 3);
+            this.bAddLayer.Location = new System.Drawing.Point(3, 3);
             this.bAddLayer.Name = "bAddLayer";
             this.bAddLayer.Size = new System.Drawing.Size(40, 40);
             this.bAddLayer.TabIndex = 0;
@@ -45,7 +47,7 @@
             // 
             // bRemoveLayer
             // 
-            this.bRemoveLayer.Location = new System.Drawing.Point(227, 3);
+            this.bRemoveLayer.Location = new System.Drawing.Point(49, 3);
             this.bRemoveLayer.Name = "bRemoveLayer";
             this.bRemoveLayer.Size = new System.Drawing.Size(40, 40);
             this.bRemoveLayer.TabIndex = 1;
@@ -62,11 +64,33 @@
             this.pLayerDisplay.Size = new System.Drawing.Size(360, 501);
             this.pLayerDisplay.TabIndex = 2;
             // 
+            // bMoveDown
+            // 
+            this.bMoveDown.Enabled = false;
+            this.bMoveDown.Location = new System.Drawing.Point(158, 3);
+            this.bMoveDown.Name = "bMoveDown";
+            this.bMoveDown.Size = new System.Drawing.Size(40, 40);
+            this.bMoveDown.TabIndex = 3;
+            this.bMoveDown.Text = "▽";
+            this.bMoveDown.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(204, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "△";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // LayerView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Crimson;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bMoveDown);
             this.Controls.Add(this.pLayerDisplay);
             this.Controls.Add(this.bRemoveLayer);
             this.Controls.Add(this.bAddLayer);
@@ -81,5 +105,7 @@
         private System.Windows.Forms.Button bAddLayer;
         private System.Windows.Forms.Button bRemoveLayer;
         private System.Windows.Forms.Panel pLayerDisplay;
+        private System.Windows.Forms.Button bMoveDown;
+        private System.Windows.Forms.Button button1;
     }
 }
