2a2b582cf88c352e32413d350087b30ae1d2f8c8
ryanflaherty <rflaherty@student.dwc.edu>
2016-12-01 02:28:28 -0500

    A

--- Paint Program/LayerView.cs
+++ Paint Program/LayerView.cs
@@ -383,7 +383,9 @@ namespace Paint_Program
             int xMod = ss.getCanvasWidth();
             int width = ss.getGridWitdh();
             
+            for (int y = 0; y < yMod; y+=width)
                 g.DrawLine(p, 0, y, yMod, y);
+            for (int x = 0; x < xMod; x+=width)
                 g.DrawLine(p, x , 0, x , xMod);
           
 
